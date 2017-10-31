4216787efb32fe2198f3f13d375ba5e9a998c5b4
alex <erman15@yandex.ru>
2017-11-01 00:42:58 +0300

    Add additional files in modules folder

---
+++ libs/modules/FindJPEG.cmake
@@ -0,0 +1,6 @@
+# Root include dir
+#find_path(JPEG_INCLUDE_DIR
+#    NAMES libjpeg/jpegtran.c)
+set(JPEG_INCLUDE_DIR "/home/alex/technopark/android/MoonMap/app/src/main/cpp/libjpeg")
+mark_as_advanced(JPEG_INCLUDE_DIR)
+
